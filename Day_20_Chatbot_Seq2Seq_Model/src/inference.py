7f119828bbc3f561fc68d7635beb7c29f8ab8c21
Shadabur-Rahaman <rahamanshadabur@gmail.com>
2025-06-04 00:46:10 +0530

    Create inference.py

---
+++ Day_20_Chatbot_Seq2Seq_Model/src/inference.py
@@ -0,0 +1,23 @@
+def decode_sequence(input_seq, encoder_model, decoder_model, tokenizer, max_len):
+    states_value = encoder_model.predict(input_seq)
+    target_seq = np.zeros((1, 1))
+    target_seq[0, 0] = tokenizer.word_index['<start>']
+
+    stop_condition = False
+    decoded_sentence = ''
+    
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_word = tokenizer.index_word.get(sampled_token_index, '')
+
+        if sampled_word == '<end>' or len(decoded_sentence) > max_len:
+            stop_condition = True
+        else:
+            decoded_sentence += ' ' + sampled_word
+
+        target_seq = np.zeros((1, 1))
+        target_seq[0, 0] = sampled_token_index
+        states_value = [h, c]
+
+    return decoded_sentence.strip()
