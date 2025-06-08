7e059569322517d6c950324f74e4c3741de2724e
Shadabur-Rahaman <rahamanshadabur@gmail.com>
2025-06-09 01:50:11 +0530

    Create dataset.py

---
+++ Day_25_Bert_text_classification/src/dataset.py
@@ -0,0 +1,32 @@
+from torch.utils.data import Dataset
+import torch
+
+class IMDBDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_len=256):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = str(self.texts[idx])
+        label = int(self.labels[idx])
+
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+
+        return {
+            'input_ids': encoding['input_ids'].squeeze(),
+            'attention_mask': encoding['attention_mask'].squeeze(),
+            'label': torch.tensor(label, dtype=torch.long)
+        }
